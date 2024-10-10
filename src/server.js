8450b16475abde3ac9625a1ef2c93036b518e85b
Jagruti kansurkar <Jagrutivk25@gmail.com>
2024-10-10 17:34:16 +0530

    onSubmit function

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ sequelize.sync({ alter: true }).then(() => {
 
 // Start the server
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
